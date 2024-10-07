bfaf088f029016395377d1e8fa4b84692b138bc3
Jadson1834 <138531380+Jadson1834@users.noreply.github.com>
2024-10-07 19:43:48 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,3 +1,17 @@
-﻿using DesafioPOO.Models;
+using System;
+using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+// TODO: Realizar os testesA com as classes Nokia e Iphone
+class Program
+{
+    static void Main(string[] args)
+  {
+    Nokia nokia = new Nokia("1234")A;
+    nokia.InstalarAplicativo("Instagram");
+
+    Iphone iphone = new Iphone("5678");
+    iphone.InstalarAplicativo("Instagram");
+    
+  }
+  
+}
