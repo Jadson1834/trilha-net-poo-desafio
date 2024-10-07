c2c8852c8204fe8280eb94322929e9c5675a9f92
Jadson1834 <138531380+Jadson1834@users.noreply.github.com>
2024-10-07 19:21:04 -0300

    Update Smartphone.cs

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -1,19 +1,30 @@
+using System;
+
 namespace DesafioPOO.Models
 {
     public abstract class Smartphone
     {
         public string Numero { get; set; }
         // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+      
+        public string Modelo {get;set;}
 
+        public string Imei {get;set;}
+
+        public int Memoria {get;set;}
+      
         public Smartphone(string numero)
         {
             Numero = numero;
+            Modelo = Modelo;
+            Imei = Imei;
+            Memoria = Memoria;
             // TODO: Passar os parâmetros do construtor para as propriedades
         }
 
         public void Ligar()
         {
-            Console.WriteLine("Ligando...");
+            Console.WriteLine("Ligan do...");
         }
 
         public void ReceberLigacao()
